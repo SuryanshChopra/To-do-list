52c06fde5f0967ab2a02262120aedbf8706baae4
Suryansh Chopra <suryanshchopra987@gmail.com>
2021-07-21 14:24:55 +0530

    created to-do

---
+++ main.js
@@ -0,0 +1,19 @@
+var addToDoButton = document.getElementById('addTodo');
+var inputText = document.getElementById('inputField');
+var addedElement = document.getElementById('toDoContainer');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('para-style');
+    paragraph.innerText = inputText.value;
+    addedElement.appendChild(paragraph);
+
+    inputText.value=""
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration="line-through"
+    })
+
+
+
+
+})
